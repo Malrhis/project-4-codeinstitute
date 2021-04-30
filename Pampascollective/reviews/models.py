61ac5cb378b62d8482a723871480bb02407a65a5
Gabriel Seet <malrhis@gmail.com>
2021-04-30 02:56:20 +0000

    create reviews model

--- Pampascollective/reviews/models.py
+++ Pampascollective/reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from products.models import Product
+import datetime
 
 # Create your models here.
+class Review(models.Model):
+    title = models.CharField(blank=False, max_length=255)
+    content = models.TextField(blank=False)
+    date = models.DateField(default=datetime.date.today)
+    # one product can have many reviews, one review can 
+    # only belong to one product
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
