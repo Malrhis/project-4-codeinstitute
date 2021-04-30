7f1d874bb4cb134aa8a9a80a4fddd46a3a31e441
Gabriel Seet <malrhis@gmail.com>
2021-04-30 06:22:59 +0000

    add reviews template

--- Pampascollective/reviews/models.py
+++ Pampascollective/reviews/models.py
@@ -1,15 +1,20 @@
+# Reviews models.py
 from django.db import models
 from products.models import Product
 import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Review(models.Model):
     title = models.CharField(blank=False, max_length=255)
     content = models.TextField(blank=False)
     date = models.DateField(default=datetime.date.today)
-    # one product can have many reviews, one review can 
+    # one product can have many reviews, one review can
     # only belong to one product
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- Pampascollective/reviews/views.py
+++ Pampascollective/reviews/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+# reviews views.py
+from django.shortcuts import (render,
+                              HttpResponse,
+                              redirect,
+                              reverse,
+                              get_object_or_404)
+from django.contrib import messages
+from .models import Review
 
 # Create your views here.
+
+
 def show_reviews(request):
-    return render(request, 'reviews/show-reviews.template.html')
+    reviews = Review.objects.all()
+    return render(request, 'reviews/show-reviews.template.html',{
+        'reviews':reviews
+    })
