087e2e0ee61953b2fd73f9ddb58eacd6b24805de
Gabriel Seet <malrhis@gmail.com>
2021-04-30 03:06:24 +0000

    add url name

--- Pampascollective/products/urls.py
+++ Pampascollective/products/urls.py
@@ -4,9 +4,12 @@ import products.views
 urlpatterns = [
     path('', products.views.show_products,
          name='show_product_route'),
-    path('create', products.views.create_product),
+    path('create', products.views.create_product,
+         name='create_product_route'),
     path('update/<product_id>',
-         products.views.update_product, name='update_product_route'),
+         products.views.update_product,
+         name='update_product_route'),
     path('delete/<product_id>',
-         products.views.delete_product, name='delete_product_route'),
+         products.views.delete_product,
+         name='delete_product_route'),
 ]
