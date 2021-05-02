26ca319c09d01b23503e5c9f4c1a0b09060ee739
Gabriel Seet <malrhis@gmail.com>
2021-05-02 16:28:07 +0000

    Add remove from cart functionality

--- Pampascollective/cart/urls.py
+++ Pampascollective/cart/urls.py
@@ -3,5 +3,7 @@ import cart.views
 
 urlpatterns = [
     path('add/<product_id>', cart.views.add_to_cart, name='add_to_cart'),
-    path('', cart.views.view_cart, name='view_cart')
+    path('', cart.views.view_cart, name='view_cart'),
+    path('remove/<product_id>', cart.views.remove_from_cart,
+         name='remove_from_cart'),
 ]
--- Pampascollective/cart/views.py
+++ Pampascollective/cart/views.py
@@ -9,6 +9,15 @@ from products.models import Product
 # Create your views here.
 
 
+def view_cart(request):
+
+    cart = request.session.get('shopping_cart', {})
+
+    return render(request, 'cart/view_cart-template.html', {
+        'cart': cart
+    })
+
+
 def add_to_cart(request, product_id):
 
     # retrieve shopping cart from session
@@ -21,7 +30,6 @@ def add_to_cart(request, product_id):
 
     if product_id in cart:
         cart[product_id]['qty'] += 1
-
 
     # create cart dictionary with product_id as the key
     else:
@@ -36,14 +44,22 @@ def add_to_cart(request, product_id):
     # save the shopping cart
     request.session['shopping_cart'] = cart
 
-    messages.success(request, (str(product.name)+" has been added to cart"))   
+    messages.success(request, (str(product.name) +
+                               " has been added to cart"))
     return redirect(reverse('show_product_route'))
 
 
-def view_cart(request):
+def remove_from_cart(request, product_id):
+    # get cart from session
+    cart = request.session.get('shopping_cart', {})
+    product = get_object_or_404(Product, pk=product_id)
 
-    cart = request.session.get('shopping_cart',{})
+    if product_id in cart:
+        # delete key from cart
+        del cart[product_id]
+        # save cart back into session
+        request.session['shopping_cart'] = cart
+        messages.success(request, (str(product.name) +
+                                   " has been removed from cart"))
 
-    return render(request, 'cart/view_cart-template.html',{
-        'cart':cart
-    })
+    return redirect(reverse('show_product_route'))
