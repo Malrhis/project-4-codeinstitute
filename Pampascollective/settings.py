89a7c9d19558eb04d13ab6ff195012a1f8110804
Gabriel Seet <malrhis@gmail.com>
2021-05-03 18:22:34 +0000

    update database settings

--- Pampascollective/settings.py
+++ Pampascollective/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,11 +122,15 @@ WSGI_APPLICATION = 'Pampascollective.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ["DATABASE_URL"])
 }
 
 # Password validation
