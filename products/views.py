f6570625588d15bdbbb0464b8868ef416e392881
Gabriel Seet <malrhis@gmail.com>
2021-05-14 02:23:06 +0000

    fix routing problem for product deletion

--- products/views.py
+++ products/views.py
@@ -117,7 +117,7 @@ def delete_product(request, product_id):
     product_to_delete = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         product_to_delete.delete()
-        return redirect(show_products)
+        return redirect(reverse(show_products))
     else:
         return render(request, 'products/delete-product.template.html', {
             "product": product_to_delete
